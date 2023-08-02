1487c711e28f13e0b2768f2ed52c97837819ba8e
PaydarNymaPC <nima.jalalabadii@gmail.com>
2023-08-03 03:08:54 +0430

    add create leave allocation

--- HR_Management.Application/DTOS/LeaveAllocation/CreateLeaveAllocationDto.cs
+++ HR_Management.Application/DTOS/LeaveAllocation/CreateLeaveAllocationDto.cs
@@ -1,10 +1,16 @@
-﻿using System;
+﻿using HR_Management.Application.DTOS.Common;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace HR_Management.Application.DTOS.LeaveAllocation
 {
-    internal class CreateLeaveAllocationDto
+    public class CreateLeaveAllocationDto : BaseDto
     {
+        public int NumberOfDays { get; set; }
+
+        public int LeaveTypeId { get; set; }
+
+        public int Priod { get; set; }
     }
 }
