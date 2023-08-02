3d0d2b7c8fc305c7503ff5de6a9314463e3236c1
PaydarNymaPC <nima.jalalabadii@gmail.com>
2023-08-02 23:51:45 +0430

    add new commands

---
+++ HR_Management.Application/Features/LeaveRequests/Handlers/Commands/CreateLeaveRequestsCommandHandler.cs
@@ -0,0 +1,19 @@
+﻿using HR_Management.Application.Features.LeaveRequests.Requests.Commands;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace HR_Management.Application.Features.LeaveRequests.Handlers.Commands
+{
+    public class CreateLeaveRequestsCommandHandler
+        : IRequestHandler<CreateLeaveRequestsCommand, int>
+    {
+        public Task<int> Handle(CreateLeaveRequestsCommand request, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ HR_Management.Application/Features/LeaveTypes/Handlers/Commands/CreateLeaveTypeCommandHandler.cs
@@ -0,0 +1,24 @@
+﻿using HR_Management.Application.Features.LeaveTypes.Requests.Commands;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace HR_Management.Application.Features.LeaveTypes.Handlers.Commands
+{
+    public class CreateLeaveTypeCommandHandler
+        : IRequestHandler<CreateLeaveTypeCommand, int>
+    {
+        public CreateLeaveTypeCommandHandler()
+        {
+                
+        }
+
+        public Task<int> Handle(CreateLeaveTypeCommand request, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
