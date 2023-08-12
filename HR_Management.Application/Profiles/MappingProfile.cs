6b5cfc7cd503282344c21d53540a76560bec5d11
PaydarNymaPC <nima.jalalabadii@gmail.com>
2023-08-12 11:53:48 +0430

    Add Infrastructure

--- HR_Management.Application/Profiles/MappingProfile.cs
+++ HR_Management.Application/Profiles/MappingProfile.cs
@@ -2,6 +2,7 @@
 using HR_Management.Application.DTOS.LeaveAllocation;
 using HR_Management.Application.DTOS.LeaveRequest;
 using HR_Management.Application.DTOS.LeaveType;
+using HR_Management.Application.Features.LeaveTypes.Requests.Commands;
 using HR_Management_Domain;
 using System;
 using System.Collections.Generic;
@@ -12,12 +13,30 @@ namespace HR_Management.Application.Profiles
     public class MappingProfile : Profile
     {
         public MappingProfile() 
-        { 
-            CreateMap<LeaveType , LeaveTypeDto>().ReverseMap();
-            CreateMap<LeaveType , CreateLeaveTypeDto>().ReverseMap();
+        {
+            #region leave trpe
+
+            CreateMap<LeaveType, LeaveTypeDto>().ReverseMap();
+            CreateMap<LeaveType, CreateLeaveTypeDto>().ReverseMap();
+
+            #endregion
+
+            #region leave request
+
             CreateMap<LeaveRequest, LeaveRequestDto>().ReverseMap();
             CreateMap<LeaveRequest, LeaveRequestListDto>().ReverseMap();
+            CreateMap<LeaveRequest, UpdateLeaveRequestDto>().ReverseMap();
+            CreateMap<LeaveRequest, CreateLeaveRequestsDto>().ReverseMap();
+
+            #endregion
+
+            #region leave allocation
+
+            CreateMap<LeaveAllocation, UpdateLeaveRequestDto>().ReverseMap();
+            CreateMap<LeaveAllocation, CreateLeaveAllocationDto>().ReverseMap();
             CreateMap<LeaveAllocation, LeaveAllocationDto>().ReverseMap();
+
+            #endregion
         }
     }
 }
