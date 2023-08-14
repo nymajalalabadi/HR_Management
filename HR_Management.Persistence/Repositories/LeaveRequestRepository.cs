7d5a7df60b3b5b93ac8dc06811ab932cea912209
PaydarNymaPC <nima.jalalabadii@gmail.com>
2023-08-14 11:41:04 +0430

    Add And Complete Repositories

---
+++ HR_Management.Persistence/Repositories/LeaveRequestRepository.cs
@@ -0,0 +1,41 @@
+﻿using HR_Management.Application.Persistence.Contracts;
+using HR_Management_Domain;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HR_Management.Persistence.Repositories
+{
+    public class LeaveRequestRepository : GenericRepository<LeaveRequest>, ILeaveRequestRepository
+    {
+        private readonly LeaveManagementDbContext _dbContext;
+        public LeaveRequestRepository(LeaveManagementDbContext dbContext) : base(dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+
+        public async Task ChangeApprovalStatus(LeaveRequest leaveRequest, bool? approvalStatus)
+        {
+            leaveRequest.Approved = approvalStatus;
+            _dbContext.Entry(leaveRequest).State = EntityState.Modified;
+            await _dbContext.SaveChangesAsync();
+        }
+
+        public async Task<List<LeaveRequest>> GetLeaveRequestsWithDetails()
+        {
+            var leaveRequsts = await _dbContext.LeaveRequests.Include(t => t.LeaveType).ToListAsync();
+
+            return leaveRequsts;
+        }
+
+        public async Task<LeaveRequest> GetLeaveRequestWithDetails(int id)
+        {
+            var leaveRequest =  await _dbContext.LeaveRequests.Include(t => t.LeaveType).FirstOrDefaultAsync(i => i.Id == id); 
+
+            return leaveRequest;
+        }
+    }
+}
