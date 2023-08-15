9e92df7532e8b796fa2854adc0278b59332fe6c6
PaydarNymaPC <nima.jalalabadii@gmail.com>
2023-08-15 17:50:26 +0430

    Add Infrastructure Services Registration

---
+++ HR_Management.Infrastructure/InfrastructureServicesRegistration.cs
@@ -0,0 +1,22 @@
+﻿using HR_Management.Application.Contracts.infrastructure;
+using HR_Management.Application.Models;
+using HR_Management.Infrastructure.Mail;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HR_Management.Infrastructure
+{
+    public static class InfrastructureServicesRegistration
+    {
+        public static IServiceCollection ConfigureInfrastructureServices(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.Configure<EmailSetting>(configuration.GetSection("EmailSettings"));
+            services.AddTransient<IEmailSender, EmailSender>();
+
+            return services;
+        }
+    }
+}
