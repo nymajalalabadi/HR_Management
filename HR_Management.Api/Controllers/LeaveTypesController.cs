2053f261904fb8f1c44caf2bea4a6bdb1b891707
PaydarNymaPC <nima.jalalabadii@gmail.com>
2023-08-21 12:10:09 +0430

    Add LeaveTypes Controller

---
+++ HR_Management.Api/Controllers/LeaveTypesController.cs
@@ -0,0 +1,54 @@
+﻿using HR_Management.Application.DTOS.LeaveType;
+using HR_Management.Application.Features.LeaveTypes.Requests.Queries;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace HR_Management.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LeaveTypesController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public LeaveTypesController(IMediator mediator)
+        {
+               _mediator = mediator;   
+        }
+
+        // GET: api/<LeaveTypesController>
+        [HttpGet]
+        public async Task<ActionResult<List<LeaveTypeDto>>> Get()
+        {
+            var leaveTypes = await _mediator.Send(new GetLeaveTypeListRequest());
+            return leaveTypes;
+        }
+
+        // GET api/<LeaveTypesController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<LeaveTypesController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<LeaveTypesController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<LeaveTypesController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
