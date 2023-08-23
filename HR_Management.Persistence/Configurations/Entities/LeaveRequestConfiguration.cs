af29e5197077c8cc10adaa65070b6e120da85cca
PaydarNymaPC <nima.jalalabadii@gmail.com>
2023-08-23 12:31:37 +0430

    Add Seed Data

---
+++ HR_Management.Persistence/Configurations/Entities/LeaveRequestConfiguration.cs
@@ -0,0 +1,15 @@
+﻿using HR_Management_Domain;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+
+namespace HR_Management.Persistence.Configurations.Entities
+{
+    public class LeaveRequestConfiguration : IEntityTypeConfiguration<LeaveRequest>
+    {
+        public void Configure(EntityTypeBuilder<LeaveRequest> builder)
+        {
+            
+        }
+    }
+}
