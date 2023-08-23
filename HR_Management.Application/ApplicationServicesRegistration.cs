18569a503ba01dd7f904328dc30668437c13fea9
PaydarNymaPC <nima.jalalabadii@gmail.com>
2023-08-23 12:48:34 +0430

    Add MediatR DependencyInjection

--- HR_Management.Application/ApplicationServicesRegistration.cs
+++ HR_Management.Application/ApplicationServicesRegistration.cs
@@ -1,18 +1,15 @@
-﻿using HR_Management.Application.Contracts.Persistence;
+﻿using MediatR;
 using Microsoft.Extensions.DependencyInjection;
-using System;
-using System.Collections.Generic;
 using System.Reflection;
-using System.Text;
 
 namespace HR_Management.Application
 {
     public static class ApplicationServicesRegistration
     {
         public static void ConfigureApplicationServices(this IServiceCollection services)
-        {
-            // services.AddAutoMapper(typeof(MappingProfile));
+        {;
             services.AddAutoMapper(Assembly.GetExecutingAssembly());
+            services.AddMediatR(Assembly.GetExecutingAssembly());
         }
     }
 }
