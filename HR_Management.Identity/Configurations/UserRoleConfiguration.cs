873ca57d0dd39ab454c313aff3a78429d9a4e9d1
PaydarNymaPC <nima.jalalabadii@gmail.com>
2023-09-06 14:20:45 +0430

    Add Identity

---
+++ HR_Management.Identity/Configurations/UserRoleConfiguration.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HR_Management.Identity.Configurations
+{
+	public class UserRoleConfiguration : IEntityTypeConfiguration<IdentityRole>
+	{
+	}
+}
---
+++ HR_Management.Identity/LeaveTypeManagementIdentityDbContext.cs
@@ -0,0 +1,20 @@
+﻿using HR_Management.Identity.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HR_Management.Identity
+{
+	public class LeaveTypeManagementIdentityDbContext : IdentityDbContext<ApplicationUser>
+	{
+		public LeaveTypeManagementIdentityDbContext(DbContextOptions<LeaveTypeManagementIdentityDbContext> options) : base(options) { }
+
+		protected override void OnModelCreating(ModelBuilder modelBuilder)
+		{
+			base.OnModelCreating(modelBuilder);
+		}
+	}
+}
+
