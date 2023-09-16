c0366438c1ab4385caa1c5574d52c04fc15a12cd
PaydarNymaPC <nima.jalalabadii@gmail.com>
2023-09-16 21:14:36 +0430

    Add AuthenticateService

--- HR_Management.MVC/Services/AuthenticateService.cs
+++ HR_Management.MVC/Services/AuthenticateService.cs
@@ -1,6 +1,9 @@
 ﻿using HR_Management.MVC.Contracts;
 using HR_Management.MVC.Services.Base;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication;
 using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
 
 namespace HR_Management.MVC.Services
 {
@@ -16,19 +19,71 @@ namespace HR_Management.MVC.Services
 			_jwtSecurityTokenHandler = new JwtSecurityTokenHandler();
 		}
 
-		public Task<bool> Authenticate(string email, string password)
+		public async Task<bool> Authenticate(string email, string password)
 		{
-			throw new NotImplementedException();
+			try
+			{
+				AuthRequest authenticateRequest = new()
+				{
+					Email = email,
+					Password = password
+				};
+				var authenticateResponse = await _client.LoginAsync(authenticateRequest);
+
+				if (authenticateResponse.Token != string.Empty)
+				{
+					var tokenContent = _jwtSecurityTokenHandler.ReadJwtToken(authenticateResponse.Token);
+					var claims = ParseClaims(tokenContent);
+					var user = new ClaimsPrincipal(new ClaimsIdentity(claims,
+						CookieAuthenticationDefaults.AuthenticationScheme));
+					var login = _httpContextAccessor.HttpContext.SignInAsync(
+						CookieAuthenticationDefaults.AuthenticationScheme, user);
+
+					_localStroge.SetStrogeValue("token", authenticateResponse.Token);
+
+					return true;
+				}
+
+				return false;
+
+
+			}
+			catch
+			{
+
+				return false;
+			}
 		}
 
-		public Task Logout()
+		public async Task Logout()
 		{
-			throw new NotImplementedException();
+			_localStroge.ClearStroge(new List<string>() { "token" });
+			await _httpContextAccessor.HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
 		}
 
-		public Task<bool> Register(string firstName, string lastName, string userName, string password)
+		public async Task<bool> Register(string firstName, string lastName, string email, string password)
 		{
-			throw new NotImplementedException();
+			RegisterationRequest registrationRequest = new()
+			{
+				Email = email,
+				Password = password,
+				FirstName = firstName,
+				LastName = lastName,
+			};
+			var response = await _client.RegisterAsync(registrationRequest);
+			if (!string.IsNullOrEmpty(response.UserId))
+			{
+				return true;
+			}
+
+			return false;
+		}
+
+		private IList<Claim> ParseClaims(JwtSecurityToken token)
+		{
+			var claims = token.Claims.ToList();
+			claims.Add(new Claim(ClaimTypes.Name, token.Subject));
+			return claims;
 		}
 	}
 }
